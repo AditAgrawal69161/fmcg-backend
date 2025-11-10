899f4a38f6e003b0b48368a28c27c5ceec166db9
Adit Agrawal <aadit69161@gmail.com>
2025-11-10 10:54:53 +0530

    Configured Render PostgreSQL database

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS, // ✅ matches .env key
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || "mysql",
-    logging: false, // set to true if you want SQL logs in console
+    dialect: process.env.DB_DIALECT || "postgres",
+    logging: false,
   }
 );
 
